6bf1ee70f816cd0268df0cda29860bb5edc641a6
MoniraMitu <mhaque44249@gmail.com>
2023-06-21 19:10:40 +0600

    code added

--- src/app/service/servi.service.ts
+++ src/app/service/servi.service.ts
@@ -9,7 +9,8 @@ export class ServiService {
   [x: string]: any;
 
   constructor(private http:HttpClient) { }
-  url = 'http://localhost:8080/api/properties';
+  // url = 'http://localhost:8080/api/properties';
+  url = 'https://theaddresspmsrestapispring-production.up.railway.app/api/properties'
 
   getAllProperty(page: any){
   return this.http.get(this.url+ '/' +"approved?page="+page);
@@ -38,7 +39,9 @@ export class ServiService {
     }
    
 
-uri='http://localhost:8080/api/bookings/';
+// uri='http://localhost:8080/api/bookings/';
+
+uri = 'https://theaddresspmsrestapispring-production.up.railway.app/api/bookings'
 
 getAll(){
   return this.http.get(this.uri);
@@ -57,16 +60,18 @@ getAll(){
     }
 
     getPropertyBySearch(area: string, category: string, type: string){
-      return this.http.get('http://localhost:8080/api/propertiesBySearch'+ '/'+ area+ '/'+ category + "/" + type);
+      return this.http.get('https://theaddresspmsrestapispring-production.up.railway.app/api/propertiesBySearch'+ '/'+ area+ '/'+ category + "/" + type);
     }
 
 
     getPropertyBySearchByArea(area: string){
-      return this.http.get('http://localhost:8080/api/propertiesBySearchByArea'+ '/'+ area);
+      return this.http.get('https://theaddresspmsrestapispring-production.up.railway.app/api/propertiesBySearchByArea'+ '/'+ area);
     }
 
 
-    urs='http://localhost:8080/api/signUps/';
+    // urs='http://localhost:8080/api/signUps/';
+
+    urs = 'https://theaddresspmsrestapispring-production.up.railway.app/api/signUp'
 
 getAllSignUp(){
   return this.http.get(this.urs);
